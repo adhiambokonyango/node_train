a38e826c2d44b560862048245790ef82b9c9ee1c
adhiambokonyango <marykonyango74@gmail.com>
2020-05-01 10:22:44 +0300

    streams

---
+++ streams.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+var data = '';
+
+// creat readable stream
+var readerStream = fs.createReadStream('input.txt');
+
+// set encoding to be utf8
+readerStream.setEncoding('UTF8');
+
+// handle stream events: data, end, error
+readerStream.on('data', function (chunk) {
+data += chunk;
+});
+
+readerStream.on('end', function () {
+console.log(data);
+});
+
+readerStream.on('error', function (err) {
+console.log(err, stack);
+});
+
+console.log("program ended");
