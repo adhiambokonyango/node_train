348b7210bffb90ad59956a549a4bce166e8e8f9a
adhiambokonyango <marykonyango74@gmail.com>
2020-05-05 10:10:36 +0300

    rest

--- rest/list_users.js
+++ rest/list_users.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var user = {
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    }
+}
+var id = 2;
+var data = require("./users.json");
 
 app.get('/listUsers', function (req, res) {
 fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
@@ -9,6 +19,40 @@ fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
 
 });
 })
+
+
+
+app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        var users = JSON.parse( data );
+        var user = users["user" + req.params.id]
+        console.log( user );
+        res.end( JSON.stringify(user));
+    });
+})
+
+app.delete('/deleteUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        delete data["user" + 2];
+
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
 var server = app.listen(8080, function (){
     var host = server.address().address
     var port = server.address().port
